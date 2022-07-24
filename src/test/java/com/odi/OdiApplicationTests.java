3a5a14c80254655f69f85b14e9ad32370f642bf7
bhowmik9202 <bhowmik9202@gmail.com>
2022-07-24 10:52:53 +0600

    first commit

---
+++ src/test/java/com/odi/OdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
